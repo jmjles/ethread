2a46af9ba4c1e1f6f0be6b57f85cb9af81a17bc2
jmjles <jmjles@gmail.com>
2024-01-26 17:39:58 -0800

    devop: moved layout to _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,17 @@
 import { Slot } from "expo-router";
 import MainLayout from "layout/main/MainLayout";
 import React, { useState } from "react";
+import { PaperProvider } from "react-native-paper";
 
 const _layout = () => {
   const [query, setQuery] = useState("");
   const handleQuery = (text: string) => setQuery(text);
   return (
-    <MainLayout {...{ value: query, onChangeText: handleQuery }}>
-      <Slot />
-    </MainLayout>
+    <PaperProvider>
+      <MainLayout {...{ value: query, onChangeText: handleQuery }}>
+        <Slot />
+      </MainLayout>
+    </PaperProvider>
   );
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, Divider, PaperProvider, Text } from "react-native-paper";
-import MainLayout from "../layout/main/MainLayout";
-import { useState } from "react";
+import { Button, Divider, Text } from "react-native-paper";
 import TileCarousel from "../components/tile/TileCarousel";
 import Post from "../components/post/Post";
 import { View } from "react-native";
 import { Threads } from "../utils/dumbydata";
 
 export default function App() {
-  const [query, setQuery] = useState("");
-  const handleQuery = (text: string) => setQuery(text);
   return (
-    <PaperProvider>
-      <MainLayout {...{ value: query, onChangeText: handleQuery }}>
-        <View>
-          <TileCarousel />
-          <Button mode="contained">
-            <Text variant="bodyMedium">Create a Post</Text>
-          </Button>
-          {Threads.map((t, i) => (
-            <View key={i}>
-              <Post {...t} />
-              {Threads.length !== i + 1 && (
-                <Divider style={{ maxWidth: 800 }} />
-              )}
-            </View>
-          ))}
+    <View>
+      <TileCarousel />
+      <Button mode="contained">
+        <Text variant="bodyMedium">Create a Post</Text>
+      </Button>
+      {Threads.map((t, i) => (
+        <View key={i}>
+          <Post {...t} />
+          {Threads.length !== i + 1 && <Divider style={{ maxWidth: 800 }} />}
         </View>
-      </MainLayout>
-    </PaperProvider>
+      ))}
+    </View>
   );
 }
